1d98ac56dbda66d6797fc7138d0465191387facb
matthieuml <merigotmatthieu@gmail.com>
2023-12-08 15:31:48 +0100

    Added new user for production & updated logs handling

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -3,8 +3,8 @@
 echo "Redis is running"
 
 echo "Starting celery..."
-celery -A api.app.celery_app worker --loglevel=warning --logfile=/var/log/celery.log --detach
-echo "Celery is running with log level warning"
+celery -A api.app.celery_app worker --loglevel=info --logfile=/var/log/celery.log --detach
+echo "Celery is running"
 
 echo "Starting server..."
 
