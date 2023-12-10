741688630b39488527396f7d94db8ed05527efbc
matthieuml <merigotmatthieu@gmail.com>
2023-12-10 14:55:17 +0100

    Added worker container for celery

--- src/api/app.py
+++ src/api/app.py
@@ -1,28 +1,14 @@
 import os
 
-from celery import Celery, Task
 from celery.result import AsyncResult
-from flask import Flask, request
+from flask import Flask
 
-from api.utils import add_together
+from api.worker.tasks import add_together
+from api.worker.initialization import celery_init_app
 
 # =============== Initialize Flask and Celery ===============
 
 app = Flask(__name__)
-
-
-def celery_init_app(app: Flask) -> Celery:
-    class FlaskTask(Task):
-        def __call__(self, *args: object, **kwargs: object) -> object:
-            with app.app_context():
-                return self.run(*args, **kwargs)
-
-    celery_app = Celery(app.name, task_cls=FlaskTask)
-    celery_app.config_from_object(app.config["CELERY"])
-    celery_app.set_default()
-    app.extensions["celery"] = celery_app
-    return celery_app
-
 
 REDIS_HOST = os.environ["REDIS_HOST"]
 REDIS_PASSWORD = os.environ["REDIS_PASSWORD"]
---
+++ src/api/worker/initialization.py
@@ -0,0 +1,20 @@
+from celery import Celery, Task
+from flask import Flask
+
+def celery_init_app(app: Flask) -> Celery:
+    """
+    Initialize Celery with Flask context.
+
+    :param app: Flask application instance
+    :return celery_app: Celery application instance
+    """
+    class FlaskTask(Task):
+        def __call__(self, *args: object, **kwargs: object) -> object:
+            with app.app_context():
+                return self.run(*args, **kwargs)
+
+    celery_app = Celery(app.name, task_cls=FlaskTask)
+    celery_app.config_from_object(app.config["CELERY"])
+    celery_app.set_default()
+    app.extensions["celery"] = celery_app
+    return celery_app
